f2f25889228a04ab0c9c17da29e0edce97a1f017
panucitelcz <petr.b257@seznam.cz>
2025-10-02 13:11:46 +0200

    Oprava

--- wp-content/themes/vlastni_sablona/page.php
+++ wp-content/themes/vlastni_sablona/page.php
@@ -4,17 +4,8 @@
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title><?php  
-            if ( is_front_page() ) { // Pokud je to domovská stránka (statická stránka)
-                echo get_the_title( get_option( 'page_on_front' ) ); // Získá název statické domovské stránky
-            } else {
-                wp_title( '|', true, 'right' ); // Standardní titulek pro ostatní stránky
-            }
-         ?></title>
 
-
-
-         <script src="<?php $theme_url?>/script/script.js" defer></script>
+         <script src="<?= $theme_url?>/script/script.js" defer></script>
          <link rel="stylesheet" href="<?php $theme_url ?>/style.css">
          <link rel="stylesheet" href="<?php $theme_url ?>/style/footer.css">
          <?php wp_head(); ?>
