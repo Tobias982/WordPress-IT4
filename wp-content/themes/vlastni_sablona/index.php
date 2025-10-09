547d41f7313876ba61257664b80b3fc9e611bd5d
Tobias982 <tobiasveverka@gmail.com>
2025-10-09 11:18:07 +0200

    bruh

--- wp-content/themes/vlastni_sablona/index.php
+++ wp-content/themes/vlastni_sablona/index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
+<?php $theme_url = get_template_directory_uri(); ?>
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?php wp_title() ?></title>
-        <link rel="stylesheet" href="<?php $theme_url ?>/style/footer.css">
+        <link rel="stylesheet" href="<?= $theme_url ?>/style/footer.css">
         <?php wp_head(); ?>
     </head>
 
